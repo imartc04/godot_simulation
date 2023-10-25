dc8e6689aca2e879d458367285185a89dd429741
imartc <imartc04@estudiantes.unileon.es>
2023-10-25 18:43:56 +0200

    wip

--- src/godot_urdf_import/urdf_python_to_tscn.py
+++ src/godot_urdf_import/urdf_python_to_tscn.py
@@ -113,30 +113,21 @@ def urdf_pos_to_tscn_transform(f_urdf_pos):
     #Create origin vector
     l_origin_vector = urdf_to_godot_vec(np.array[f_urdf_pos.x, f_urdf_pos.y, f_urdf_pos.z])
 
-    return f'''{l_final_rotation_matrix[0,0]}, {l_final_rotation_matrix[1,0]}, {l_final_rotation_matrix[2,0]} , {l_final_rotation_matrix[0,1]} ,{l_final_rotation_matrix[1,1]}, {l_final_rotation_matrix[2,1]}, {l_final_rotation_matrix[2,0]} , {l_final_rotation_matrix[2,1]}, {l_final_rotation_matrix[2,2]}, {l_origin_vector[0]}, {l_origin_vector[1]}, {l_origin_vector[2]}'''          
+    return f'''transform = Transform3D({l_final_rotation_matrix[0,0]}, {l_final_rotation_matrix[1,0]}, {l_final_rotation_matrix[2,0]} , {l_final_rotation_matrix[0,1]} ,{l_final_rotation_matrix[1,1]}, {l_final_rotation_matrix[2,1]}, {l_final_rotation_matrix[2,0]} , {l_final_rotation_matrix[2,1]}, {l_final_rotation_matrix[2,2]}, {l_origin_vector[0]}, {l_origin_vector[1]}, {l_origin_vector[2]})'''          
 
 def get_filename_and_ext_from_path(f_path):
     return os.path.splitext(os.path.basename(f_path))
 
 
 
-def genrate_link_visual_nodes(f_link_visuals, f_sub_resources, f_extern_resources, f_out_dir):
+def genrate_link_visual_nodes(f_link_visuals, f_tscn_body_str, f_sub_resources_str, f_extern_resources_str, f_global_id_ctr,  f_out_dir, f_parent_str):
 
     for  link_data in f_link_visuals:
             
             #Create Nodes for the link visuals
             l_visual_counter = 0
             for visual in link_data.visuals:
-                l_visual_node = Node("visual", data=visual, parent=l_link_node)
-                
-                #Generate tscn string for the visual
-                l_tscn_header = '[node name=' + visual.name + '"parent="' + key + '"'
 
-                l_tscn_props = urdf_pos_to_tscn_transform(visual.origin)
-
-                l_geometry_id = ""
-
-                l_visual_node_str = ""
 
                 #Check visual geometry type
                 if visual.geometry.type == "mesh":
@@ -160,66 +151,65 @@ def genrate_link_visual_nodes(f_link_visuals, f_sub_resources, f_extern_resource
 
                     l_mesh_proj_relative =  "res://" + os.path.basename(l_mesh_file_name)
 
-                    l_geometry_id = l_visual_node.name + '_visual_' + str(l_visual_counter) + '_mesh_shape'
+                    l_geometry_id = f'{visual.name}_visual_{f_global_id_ctr[0]}_mesh_shape'''
 
-                    l_external_resources_str += '\n[sub_resource type="PackedScene" id="' + l_geometry_id  + '"'  + ' uid="uid://' + l_geometry_id + '"' + 'path="' + l_mesh_proj_relative + '" ]'
-
+                    f_extern_resources_str += f'''\n[ext_resource type="PackedScene" id="{l_geometry_id} uid="uid://{l_geometry_id}" path="{l_mesh_proj_relative}"]'''
 
                     l_node_name = "node_" + l_geometry_id
 
-                    l_visual_node_str = '\n[node name="' + l_node_name + '" parent="' + link_data.name + '"' + 'instance=ExtResource("' + l_geometry_id + '")]'
-                    
+                    f_tscn_body_str += f'''\n[node name="{l_node_name} parent="{f_parent_str}" instance=ExtResource("{l_geometry_id}")]'''
+                                    
 
-                    
                 elif visual.geometry.type == "box":
 
-                    l_geometry_id = l_visual_node.name + '_visual_' + str(l_visual_counter) + '_box_shape'
-                
-                    l_sub_resources_str += '\n[sub_resource type="BoxShape3D" id="' + l_geometry_id  + '"]'  
+                    l_geometry_id = f'''{visual.name}_visual_{f_global_id_ctr[0]}_box_shape'''
+                    l_node_name = f'''node_{l_geometry_id}'''
 
-                    l_sub_resources_str += '\nsize= Vector3(' + str(visual.geometry.data['size'][0]) + ', ' + str(visual.geometry.data['size'][1]) + ', ' + str(visual.geometry.data['size'][2]) + ')'
+                    f_sub_resources_str  += f'''\n[sub_resource type="BoxShape3D" id="{f_global_id_ctr[0]}"]  
+                    '\nsize= Vector3({visual.geometry.data['size'][0]}, {visual.geometry.data['size'][1]}, {visual.geometry.data['size'][2]})'''
 
                     #Create visual node string for the resource
-                    l_visual_node_str = '\n[node name="' + l_node_name + ' type="MeshInstance3D"' '" parent="' + link_data.name +'")]'
-                    l_visual_node_str += '\nmesh= SubResource("' + l_geometry_id + '")'
+                    f_tscn_body_str = f'''\n[node name="{l_node_name} type="MeshInstance3D" parent="{f_parent_str}"]
+                     shape= SubResource("{l_geometry_id}")'''
+                    
 
                 elif visual.geometry.type == "cylinder":
 
-                    l_geometry_id = l_visual_node.name + '_visual_' + str(l_visual_counter) + '_cylinder_shape'
+                    l_geometry_id = f'''{visual.name}_visual_{f_global_id_ctr[0]}_cylinder_shape'''
+                    l_node_name = f'''node_{l_geometry_id}'''
 
-                    l_sub_resources_str += '\n[sub_resource type="CylinderShape3D" id="' + l_geometry_id  + '"]'
-
-                    l_sub_resources_str += '\nradius= ' + str(visual.geometry.data['radius'])
-                    l_sub_resources_str += '\nheight= ' + str(visual.geometry.data['length'])
-
-
+                    f_sub_resources_str += f'''\n[sub_resource type="CylinderShape3D" id="{l_geometry_id}"]
+                    radius={visual.geometry.data['radius']})
+                    height={visual.geometry.data['length']})'''
+                    
                     #Create visual node string for the resource
-                    l_visual_node_str = '\n[node name="' + l_node_name + ' type="MeshInstance3D"' '" parent="' + link_data.name +'")]'
-                    l_visual_node_str += '\nmesh= SubResource("' + l_geometry_id + '")'
+                    f_tscn_body_str = f'''\n[node name="{l_node_name} type="MeshInstance3D parent={f_parent_str})]
+                    nmesh= SubResource("{l_geometry_id}")'''
 
 
                 elif visual.geometry.type == "sphere":
 
-                    l_geometry_id = l_visual_node.name + '_visual_' + str(l_visual_counter) + '_sphere_shape'
+                    l_geometry_id = f'''{visual.name}_visual_{f_global_id_ctr[0]}_sphere_shape'''
+                    l_node_name = f'''node_{l_geometry_id}'''
 
-                    l_sub_resources_str += '\n[sub_resource type="SphereShape3D" id="' + l_geometry_id  + '"]'
-
-                    l_sub_resources_str += '\nradius= ' + str(visual.geometry.data['radius'])
+                    f_sub_resources_str += f'''\n[sub_resource type="SphereShape3D" id="{l_geometry_id}"]
+                    radius={visual.geometry.data['radius']}
+                    '''
 
                     #Create visual node string for the resource
-                    l_visual_node_str = '\n[node name="' + l_node_name + ' type="MeshInstance3D"' '" parent="' + link_data.name +'")]'
-                    l_visual_node_str += '\nmesh= SubResource("' + l_geometry_id + '")'
+                    f_tscn_body_str += f'''\n[node name="{l_node_name} type="MeshInstance3D" parent="{f_parent_str}"]
+                    mesh= SubResource("{l_geometry_id}")'''
+
 
                 else:
                     raise ValueError("Invalid geometry type specified. Must be 'mesh', 'box', 'cylinder' or 'sphere'")
 
 
-                #Create MeshInstance node string for the resource
-                l_visual_node = Node(l_node_name, node_str=l_visual_node_str , parent=l_link_node)
+                #Add visual origin 
+                f_tscn_body_str += f'{urdf_pos_to_tscn_transform(visual.origin)}'
 
-
-            #Add the link node to the dictionary
-            l_link_nodes[key] = l_link_node
+                #Increment global id counter
+                f_global_id_ctr[0] +=1
 
 
 
@@ -238,15 +228,102 @@ mass = {f_mass_str}
     return l_ret
 
 
-def generate_link_nodes(f_urdf_python_links, f_out_dir, f_sub_resources_str, f_extern_resources_str, tscn_body_str, f_obj_parent_tree, f_mass_center_mode):
+
+def generate_collision_nodes(f_urdf_python_col, f_parent_str, f_global_id_ctr, f_out_dir, f_sub_resources_str, f_extern_resources_str, f_tscn_body_str, f_obj_parent_tree):
+
+    for col_obj in f_urdf_python_col:
+
+        #Create collision shape object
+        f_tscn_body_str += f'''[node name="CollisionShape3D" type="CollisionShape3D" parent="{f_parent_str}"]
+        {urdf_pos_to_tscn_transform(col_obj.origin)}
+        '''
+
+        l_geom = col_obj.geometry
+
+
+        #Create sub resource shape 
+
+        if l_geom.type == "mesh":
+        
+            #Get mesh and generate convex polygon shape from it
+            l_mesh_file_name = l_geom.data['filename']
+
+            l_file_name, l_ext = get_filename_and_ext_from_path(l_mesh_file_name)
+
+            #Check if the mesh file is an stl file
+            if l_ext == ".stl":
+                #Convert stl to dae
+                stl_to_dae(l_mesh_file_name, f_out_dir + "/" + l_file_name + ".dae")
+
+            elif l_ext == ".dae":
+                #Valid format, nothing to do
+                pass
+
+            else:
+                raise ValueError("Invalid mesh file extension. Must be '.stl' or '.dae'")
+
+
+            l_mesh_proj_relative =  "res://" + os.path.basename(l_mesh_file_name)
+
+            l_geometry_id = f'{l_geom.name}_visual_{f_global_id_ctr[0]}_mesh_shape'''
+
+            f_extern_resources_str += f'''\n[ext_resource type="PackedScene" id="{l_geometry_id} uid="uid://{l_geometry_id}" path="{l_mesh_proj_relative}"]'''
+
+            l_node_name = "node_" + l_geometry_id
+
+            f_tscn_body_str += f'''\n[node name="{l_node_name} parent="{f_parent_str}" instance=ExtResource("{l_geometry_id}")]'''
+                        
+
+        elif l_geom.type == "box":
+
+            l_geometry_id = f'''{l_geom.name}_visual_{f_global_id_ctr[0]}_box_shape'''
+            l_node_name = f'''node_{l_geometry_id}'''
+
+            f_sub_resources_str  += f'''\n[sub_resource type="BoxShape3D" id="{f_global_id_ctr[0]}"]  
+            '\nsize= Vector3({l_geom.data['size'][0]}, {l_geom.data['size'][1]}, {l_geom.data['size'][2]})'''
+            
+
+        elif l_geom.type == "cylinder":
+
+            l_geometry_id = f'''{l_geom.name}_visual_{f_global_id_ctr[0]}_cylinder_shape'''
+            l_node_name = f'''node_{l_geometry_id}'''
+
+            f_sub_resources_str += f'''\n[sub_resource type="CylinderShape3D" id="{l_geometry_id}"]
+            radius={l_geom.data['radius']})
+            height={l_geom.data['length']})'''
+            
+
+        elif l_geom.type == "sphere":
+
+            l_geometry_id = f'''{l_geom.name}_visual_{f_global_id_ctr[0]}_sphere_shape'''
+            l_node_name = f'''node_{l_geometry_id}'''
+
+            f_sub_resources_str += f'''\n[sub_resource type="SphereShape3D" id="{l_geometry_id}"]
+            radius={l_geom.data['radius']}
+            '''
+
+        else:
+            raise ValueError("Invalid geometry type specified. Must be 'mesh', 'box', 'cylinder' or 'sphere'")
+
+
+            
+
+
+        
+
+def generate_link_nodes(f_urdf_python_links, f_global_id_ctr, f_out_dir, f_sub_resources_str, f_extern_resources_str, f_tscn_body_str, f_obj_parent_tree, f_mass_center_mode):
 
     """
-    Each link will be represented by a Godot rigid body.
-    The rigid body will have a set of 3D meshes, a set of collision objects and
-    optionally a set of associated joints
-
+    Each link will be represented by a node3D.
+    The node3D has will have a Godot rigid body, a set of 3D meshes,
+    a set of collision objects and optionally a set of associated joints
     """
     for key, link_data in f_urdf_python_links.items():
+
+
+        #Create node3D
+        f_tscn_body_str += f'[node name="{link_data.name}" type="Node3D" parent="{f_obj_parent_tree[link_data.name]}"]'
+
 
         """
         Create link rigid body
@@ -255,11 +332,39 @@ def generate_link_nodes(f_urdf_python_links, f_out_dir, f_sub_resources_str, f_e
         to the othe the solution adopted is to set the transform3D of the Godot rigid body as the URDF inertial system with a 
         vector with center of mass of (0, 0, 0)
         """
-        tscn_body_str += create_godot_rigid_body_str(link_data.name, f_obj_parent_tree[link_data.name], urdf_pos_to_tscn_transform(link_data.inertial.pose),link_data.inertial.mass)
+        f_tscn_body_str += create_godot_rigid_body_str(f'{link_data.name}_rigid_body', f'{f_obj_parent_tree[link_data.name]}/{link_data.name}', urdf_pos_to_tscn_transform(link_data.inertial.pose),link_data.inertial.mass)
 
-        genrate_link_visual_nodes(link_data.visuals, l_sub_resources, l_external_resources, f_out_dir)
+        #Create visual nodes
+        genrate_link_visual_nodes(link_data.visuals, f_tscn_body_str, f_sub_resources_str, f_extern_resources_str, f_global_id_ctr, f_out_dir, f'{f_obj_parent_tree[link_data.name]}/{link_data.name}')
 
-    return l_link_nodes
+        #Create collision nodes
+
+
+
+def gen_joints(f_joints, f_out_dir, f_obj_parent_tree):
+
+    for key, joint_data in f_joints.items():
+
+        #Create node based on the joint type
+        if joint_data.type == "revolute": 
+
+            """
+            Godot hinge joint only rotates around X so if the revolution axis
+            
+            """
+
+
+        elif joint_data.type == "continuous": 
+
+        elif joint_data.type == "prismatic": 
+
+        elif joint_data.type == "fixed": 
+
+        elif joint_data.type == "floating": 
+        
+
+        elif joint_data.type == "planar": 
+
 
 
 
